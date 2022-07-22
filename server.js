8494922a371d49d03d89a5ce582d2c70624204af
anrypwnz <anrypwnz@gmail.com>
2022-07-22 13:33:00 +0300

    Исправления, согласно проведенному ревью

--- server.js
+++ server.js
@@ -1,26 +1,18 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+require("dotenv").config();
 
-const data = require('./static/testData')
-const app = express()
-const PORT = 3000
+const app = express();
+let PORT = process.env.PORT || 3000
 
-app.set('view engine', 'hbs')
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-  return res.render(path.resolve(__dirname + '/dist/index.html'), data)
+  return res.render(path.resolve(__dirname + '/dist/index.html'));
 });
 
-app.use(express.static('dist'))
-
-app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, '/dist/404.html'))
-})
-
-app.use((req, res) => {
-  res.status(500).sendFile(path.join(__dirname, '/dist/500.html'))
-})
+app.use(express.static('dist'));
 
 app.listen(PORT, () => {
-  console.log(`Starting server on port ${PORT}`)
+  console.log(`Starting server on port ${PORT}`);
 });
